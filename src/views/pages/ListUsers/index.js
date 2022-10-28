9a2d440c1b4057d65e1d0d3f9a776ad3d8c5d739
Vinicius Leal <56416018+viniciusleal34@users.noreply.github.com>
2022-10-28 10:04:55 -0300

    feat: vote yourself

--- src/views/pages/ListUsers/index.js
+++ src/views/pages/ListUsers/index.js
@@ -3,15 +3,14 @@ import React, { useState, useEffect, useContext, useRef } from "react";
 import { toast } from "react-toastify";
 import Button from "../../../components/Button";
 import { AuthContext } from "../../../contexts/UserContext";
-import api from "../../../services/api";
-import { logout } from "../../../services/auth";
+import { getUser, logout } from "../../../services/auth";
 
 import { Container } from "./styles";
 
 function ListagemUser() {
   const { getAllCandidateAsync } = useContext(AuthContext);
-  const materialRef = useRef(null);
   const [candidateVote, setCandidateVote] = useState([]);
+  const [candidateSelect, setCandidateSelect] = useState(false);
   const [users, setUsers] = useState([]);
   const [columns, setColumns] = useState([
     { title: "Nome", field: "nome" },
@@ -21,8 +20,9 @@ function ListagemUser() {
   const getAllCandidate = async () => {
     try {
       const resp = await getAllCandidateAsync();
-      if (resp) {
-        setUsers(() => resp);
+      const candidatos = resp.filter(item => item.matricula != JSON.parse(getUser()).userName)
+      if (candidatos) {
+        setUsers(() => candidatos);
         return true;
       }
       logout();
@@ -32,25 +32,39 @@ function ListagemUser() {
     }
   };
 
+  const selectYourself = (value) => {
+    if (value) {
+      const matricula = JSON.parse(getUser()).userName;
+      setCandidateSelect({ matricula });
+    } else {
+      setCandidateSelect(false);
+    }
+  };
+
   const sendVote = async () => {
-    try{
-      if (candidateVote.length > 3) {
+    try {
+      if (candidateVote.length > 2 && candidateSelect != false) {
+        toast.error("Voce deve selecionar apenas 3 candidatos");
+      } else if (candidateVote.length > 3) {
         toast.error("Voce deve selecionar apenas 3 candidatos");
         return false;
       }
       const bodyVote = candidateVote.map((item) => {
         return { matricula: item.matricula.toString() };
       });
-      const resp = await api.post("/user/saveVote", {
-        votos: bodyVote,
-      });
-      if(resp.status == 200){
-        toast.success('Seu voto foi feito com sucesso!')
-      }
-  
-    }catch(err){
-      console.error(err?.response?.data)
-      toast.error(err?.response?.data?.message)
+      if (candidateSelect != false) {
+        bodyVote.push(candidateSelect);
+      } 
+      console.log(bodyVote)
+      // const resp = await api.post("/user/saveVote", {
+      //   votos: bodyVote,
+      // });
+      // if (resp.status == 200) {
+      //   toast.success("Seu voto foi feito com sucesso!");
+      // }
+    } catch (err) {
+      console.error(err?.response?.data);
+      toast.error(err?.response?.data?.message);
     }
   };
 
@@ -60,18 +74,37 @@ function ListagemUser() {
 
   return (
     <Container>
+      <header>
+        <input
+          type="checkbox"
+          onChange={(e) => selectYourself(e.target.checked)}
+        />
+        <div>Votar em mim mesmo</div>
+      </header>
       <MaterialTable
-      style={{width:'90%'}}
+        style={{ width: "90%" }}
         columns={columns}
         data={users}
         title="Candidatos"
         onSelectionChange={(rows) => {
-          if (rows.length > 3) {
+          if (candidateSelect != false && rows.length > 2) {
+            toast.error("Selecione apenas 3 candidatos");
+          } else if (rows.length > 3) {
             toast.error("Selecione apenas 3 candidatos");
           }
           setCandidateVote(() => rows);
         }}
         options={{ search: true, actionsColumnIndex: -1, selection: true }}
+        localization={{
+          pagination: {
+            labelRowsSelect: "Nº de candidatos",
+            labelDisplayedRows: "aa",
+          },
+          toolbar: {
+            searchPlaceholder: "Pesquisar",
+            nRowsSelected: `${candidateVote.length} candidatos selecionados`,
+          },
+        }}
       />
       <Button
         style={{ width: 100, alignSelf: "flex-start", marginLeft: "15%" }}
--- src/views/pages/ListUsers/styles.js
+++ src/views/pages/ListUsers/styles.js
@@ -7,4 +7,19 @@ export const Container = styled.div`
   width:100%;
   justify-content: center;
   margin-top: 20px;
+  header {
+    display: flex;
+    width: 90%;
+    flex-direction: row;
+    background-color: #fff;
+    height: 100px;
+    align-items: center;
+    > input{
+      width: 20px;
+      height: 20px;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 20px;
+      margin: 0px 10px;
+    }
+  }
 `;
